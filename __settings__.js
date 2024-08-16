23faa865be953f836c7ddba15817de3f8f540e4e
Maggie Condit <maggiecondit@Maggies-MacBook-Pro.local>
2024-08-15 22:00:55 -0400

    emma campbell

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2045617.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 190946984, 191930701, 192096392, 192097352 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/191930703/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/191930704/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/191930702/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/191925963/1/basis.wasm.js', 'wasmUrl' : 'files/assets/191925961/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/191925962/1/basis.js', 'preload' : false},
+];
